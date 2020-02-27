4514d589b1658f8daba3ceb192ea09850979d04e
Alex <Magana@Alexs-MacBook-Air.local>
2020-02-26 22:51:38 -0600

    initial commit

--- replate/src/App.js
+++ replate/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Driver from './components/Driver';
+import Business from './components/Business';
 
 function App() {
   return (
     <div className="App">
-      
+      <Login />
     </div>
   );
 }
