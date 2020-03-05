9332e8659b6d2e6670f73f9b741f52034588235b
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-05 13:16:25 -0500

    FEATURE:(Driver): - mapped available pick ups under "Add Below Stops?" - coded ability to add pick up to schedule

--- replate/src/components/Driver/Driver.js
+++ replate/src/components/Driver/Driver.js
@@ -1,23 +1,120 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextInput, Button, Label } from "evergreen-ui";
 import Location from '../Location/Location';
+import {axiosWithAuth} from "../../utilities/axiosWithAuth";
+import {useParams} from "react-router";
 
 const Driver = () => {
+
+    const {username} = useParams();
+    console.log("?: ", username);
+
+    const [pickUp, setPickUp] = useState([]);
+    const [schedule, setSchedule] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+          .get('/api/pickups')
+          .then(response => {
+              console.log("Response: ", response.data)
+              setPickUp(response.data)
+          })
+          .catch(error => {
+              console.log("What's the hold up? ", error)
+          });
+        axiosWithAuth()
+          .get(`/api/user/driver/${username}`)
+          .then(response => {
+              console.log("Driver: ", response)
+          })
+          .catch(error => {
+              console.log("What's the matter? ", error)
+          })
+    }, []);
+
+    const addPickUp = location => {
+        setSchedule([...schedule, location])
+    };
 
     return (
         <div className="container">
             <div className="business-home">
                 <h2>Hello {}</h2>
+                <section className="section-container">
+                    <div className="content content-map">
+                        <div className="map-content">
+                            <Location />
+                        </div>
+                        <div className="map-actions">
+                            <ul>
+                                <li><Button className="">Call Contact</Button></li>
+                                <li><Button className="">Call Helpline</Button></li>
+                                <li><Button className="">Parking Toggle</Button></li>
+                                <li><Button className="">Center Map</Button></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="content content-schedule">
+                        <div className="content-header header-schedule">
+                            <h2>Your Schedule</h2>
+                        </div>
+                        <div className="content-body body-schedule">
+                            <ul>
+                                {schedule.map(location => {
+                                    console.log("Location: ", location)
+                                    return (
+                                      <li key={location.id}>
+                                          <div>{location.pickupTime}</div>
+                                          <div>
+                                              <div>{location.food}</div>
+                                              <div>address</div>
+                                          </div>
+                                          <div>
+                                            <span>{location.index}</span>
+                                          </div>
+                                      </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="content content-pickups">
+                        <div className="content-header header-pickups">
+                            <h2>Add stops below?</h2>
+                            <span><Button>Add</Button></span>
+                        </div>
+                        <div className="content-body body-pickups">
+                            <ul>
+                            {pickUp.map(location => {
+                                console.log("Location: ", location)
+                                return (
+                                  <li key={location.id}>
+                                      <div>{location.pickupTime}</div>
+                                      <div>
+                                          <div>{location.food}</div>
+                                          <div>address</div>
+                                      </div>
+                                      <div>
+                                          <Button
+                                            onClick={() => {
+                                                addPickUp(location)
+                                            }}
+                                          >
+                                              Pickup / Stop
+                                          </Button>
+                                      </div>
+                                  </li>
+                                )
+                            })}
+                            </ul>
+                        </div>
+                    </div>
+                </section>
                 <div className="driverNav">
                     {/* google map api */}
-                    <ul>
-                        <li><Button className="">Call Contact</Button></li>
-                        <li><Button className="">Call Helpline</Button></li>
-                        <li><Button className="">Parking Toggle</Button></li>
-                        <li><Button className="">Center Map</Button></li>
-                    </ul>
-                    <div className="row">
-                        <h2>Your Schedule</h2>
+
+                     <div className="row">
+
                         <a>Idle</a>
                     </div>
                     <div className="row">
@@ -49,7 +146,7 @@ const Driver = () => {
                         <p>business<br />business street</p>
                         <Button>Pickup/Stop</Button>
                     </div>
-                    <Location />
+
                 </div>
             </div>
         </div>
