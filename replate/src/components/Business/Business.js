4ba9a2a5c0afe38b8b34c0adf33125cf992b1016
ConfusionOrb221 <nate1898@gmail.com>
2020-03-05 00:26:19 -0500

    Pushing some new features iwht business

--- replate/src/components/Business/Business.js
+++ replate/src/components/Business/Business.js
@@ -1,45 +1,118 @@
-import React from 'react';
-import { TextInput, Button, Label } from "evergreen-ui";
+import React, { Component } from 'react';
+import Location from '../Location/Location';
+import { Button } from "evergreen-ui";
+import {axiosWithAuth} from '../../utilities/axiosWithAuth';
 
-export default function Business() {
-    return (
-        <div className="container">
-            <div className="business-home">
-                <h2>Hello {}</h2>
-                <div className="driverNav">
-                    {/* google map api */}
-                    <ul>
-                        <li><Button className="">Call Contact</Button></li>
-                        <li><Button className="">Call Helpline</Button></li>
-                        <li><Button className="">ETA</Button></li>
-                        <li><Button className="">Center Map</Button></li>
-                    </ul>
-                    <div className="business-current-pickup">
-                        <div className="pickup-info">
-                            <h3>Today 8:00 pm</h3>
-                            <p>Less...</p>
-                            <p>Pickup acquired. Driver is in transit. ETA approx. 12 minutes</p>
-                        </div>
-                        <div className="business-actions">
-                            <Button>Edit</Button>
-                            <Button>Cancel</Button>
-                        </div>
-                    </div>
-                    <div className="business-future-pickup">
-                        <div className="business-future-pickup-info">
-                            <h3>Date Time</h3>
-                        </div>
-                        <div className="business-future-actions">
-                            <Button>Edit</Button>
-                            <Button>Cancel</Button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <h3>Schedule New</h3>
-                        <Button>Click Here</Button>
-                    </div>
+class Business extends Component{
+
+    state = {
+        pickup: {
+            food: '',
+            amount: '',
+            description: '',
+            pickupTime: '',
+        },
+        showPickup: false
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentWillUnmount(){
+        //localStorage.removeItem('token');
+    }
+
+    handleChangePickup = e =>{
+        this.setState({
+			...this.state,
+            pickup: {
+                ...this.state.pickup,
+                [e.target.name]: e.target.value
+            }
+		});
+    }
+
+    togglePickup = e =>{
+        this.setState({
+            ...this.state,
+            showPickup: !this.state.showPickup
+        })
+    }
+
+    submitPickup = e =>{
+        e.preventDefault();
+        let dateControl = document.querySelector('input[type="date"]');
+        let timeControl = document.querySelector('input[type="time"]');
+
+        let dateTime = `${dateControl.value}/${timeControl.value}`;
+
+        axiosWithAuth()
+        .post('/api/pickups', {
+            food: this.state.pickup.food,
+            amount: this.state.pickup.amount,
+            description: this.state.pickup.description,
+            pickupTime: dateTime
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => console.log(err));
+        
+        console.log(dateControl.value);
+    }
+
+    render(){
+        console.log(this.context);
+        return (
+            <div className="container">
+                <div className="business-home">
+                <button onClick={this.togglePickup}> {!this.state.showPickup ? <> Create Pickup </> : <> Hide Pickup </>} </button>
+                        { !this.state.showPickup ? <> </> : <form onSubmit={this.submitPickup}>
+                            <label> Food: </label>
+                            <input 
+                                type="text"
+                                name="food"
+                                value={this.state.pickup.food}
+                                onChange={this.handleChangePickup}
+                                required
+                            />
+                            <label> amount: </label>
+                            <input 
+                                type="text"
+                                name="amount"
+                                value={this.state.pickup.amount}
+                                onChange={this.handleChangePickup}
+                                required
+                            />
+                            <label> description: </label>
+                            <input 
+                                type="text"
+                                name="description"
+                                value={this.state.pickup.description}
+                                onChange={this.handleChangePickup}
+                                required
+                            />
+                            <label> Date: </label>
+                            <input 
+                                type="date"
+                                name="pickupTime"
+                                required
+                            />
+                            <label> Time: </label>
+                            <input 
+                                type="time"
+                                name="pickupTime"
+                                step='600'
+                                required
+                            />
+                            <input type="submit" value='Submit' />
+                        </form>
+                        }
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default Business;
